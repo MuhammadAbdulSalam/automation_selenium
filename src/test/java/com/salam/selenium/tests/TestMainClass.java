334fc704eb4379edf0ddd03dd2ff3694bd314bf8
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2022-11-29 16:18:02 +0000

    initial commit

---
+++ src/test/java/com/salam/selenium/tests/TestMainClass.java
@@ -0,0 +1,15 @@
+package com.salam.selenium.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestMainClass {
+
+    @Test
+    public void setUp() throws Exception {
+        System.out.println("Something");
+    }
+
+}
