1b119217d2d0cb3f9392d418646f41aed242bd42
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2022-12-01 21:21:18 +0000

    lesson 2

--- src/test/java/com/salam/selenium/tests/TestMainClass.java
+++ src/test/java/com/salam/selenium/tests/TestMainClass.java
@@ -1,19 +1,33 @@
 package com.salam.selenium.tests;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @RunWith(JUnit4.class)
 public class TestMainClass {
 
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "/Users/muhammadabdulsalam/Dev/intelij/automation_selenium/chromedriver");
+    }
+
+    @Test
+    public void runMyBrowser(){
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.google.co.uk/");
+    }
+
     @Test
     public void myFirstTest() throws Exception {
         ArrayList<String> numberArray = new ArrayList<String>();
-        numberArray.add("one"); // 0
+        numberArray.add("salam"); // 0
         numberArray.add("two"); // 1
         numberArray.add("three");
         numberArray.add("four");
@@ -26,6 +40,8 @@ public class TestMainClass {
         for (int i = 0; i < 9; i = i+2){
             System.out.println(numberArray.get(i));
         }
+
+        Assert.assertNotEquals(numberArray.get(0), "idiot");
 
     }
 
