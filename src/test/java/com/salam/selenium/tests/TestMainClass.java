fe2d3a47b4a6229ee9bdc18ebb3e21bee433cdcc
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2022-12-05 21:22:35 +0000

    lesson three

--- src/test/java/com/salam/selenium/tests/TestMainClass.java
+++ src/test/java/com/salam/selenium/tests/TestMainClass.java
@@ -1,11 +1,14 @@
 package com.salam.selenium.tests;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
@@ -13,36 +16,28 @@ import java.util.ArrayList;
 @RunWith(JUnit4.class)
 public class TestMainClass {
 
+    ChromeDriver chromeDriver= new ChromeDriver();
+
     @Before
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "/Users/muhammadabdulsalam/Dev/intelij/automation_selenium/chromedriver");
+        chromeDriver.get("https://amaninja-concept2.myshopify.com/password");
     }
+
 
     @Test
-    public void runMyBrowser(){
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://www.google.co.uk/");
+    public void runMyBrowser() throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement passwordField = chromeDriver.findElement(By.id("password"));
+        passwordField.sendKeys("anything!!");
+        Thread.sleep(3000);
     }
 
-    @Test
-    public void myFirstTest() throws Exception {
-        ArrayList<String> numberArray = new ArrayList<String>();
-        numberArray.add("salam"); // 0
-        numberArray.add("two"); // 1
-        numberArray.add("three");
-        numberArray.add("four");
-        numberArray.add("five");
-        numberArray.add("six");
-        numberArray.add("seven");
-        numberArray.add("eight");
-        numberArray.add("nine"); //8
 
-        for (int i = 0; i < 9;  i++){
-            System.out.println(numberArray.get(i));
-        }
-
-        Assert.assertNotEquals(numberArray.get(0), "idiot");
-
+    @After
+    public void exitChrome(){
+        chromeDriver.quit();
     }
+
 
 }
