13498ac12feef405eafe170857c730b2cd758458
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2022-12-12 21:23:31 +0000

    lesson 5 data tables and data classes/objects

--- src/test/java/com/salam/selenium/tests/CucumberTestsExample.java
+++ src/test/java/com/salam/selenium/tests/CucumberTestsExample.java
@@ -1,8 +1,12 @@
 package com.salam.selenium.tests;
 
 
+import com.salam.selenium.utils.User;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+
+import java.util.ArrayList;
 
 public class CucumberTestsExample {
 
@@ -10,17 +14,54 @@ public class CucumberTestsExample {
     public void my_browser_is_up_and_running_and_website_is_loaded(Integer int1) {
 
     }
-    @Then("Go to site Enter Username {string}")
-    public void go_to_site_enter_username(String userName) {
-        System.out.println(userName);
-    }
-    @Then("Enter Password {string} and {string}")
-    public void enter_password_and(String string, String string2) {
 
-    }
-    @Then("validate success login")
-    public void validate_success_login() {
+    @Then("Test using following data")
+    public void test_using_following_data(DataTable dataTable) {
+        int numberOfRows = dataTable.height();
+        int numberOfColumns = dataTable.width();
 
+        //how to read elements one by one
+        String firstNameFirstRow = dataTable.row(0).get(0);
+        String secondNameFirstRow = dataTable.row(0).get(1);
+
+        System.out.println("First Name : " + firstNameFirstRow + " ,Second Name: " + secondNameFirstRow);
+
+        //For loop to run through data tables
+        for (int i = 0; i < dataTable.height(); i++){
+
+            String firstName = dataTable.row(i).get(0);
+            String secondName = dataTable.row(i).get(1);
+            String email = dataTable.row(i).get(2);
+
+            System.out.println("FirstName: " + firstName);
+            System.out.println("SecondName: " + secondName);
+            System.out.println("email: " + email);
+            System.out.println("---From First Loop---");
+        }
+
+        //example how to create new objects
+        User myUser = new User("abdul", "salam", "test@test.com");
+
+        //create a list of users so i can add my user objects to it
+        ArrayList<User> myUsersList = new ArrayList<User>();
+
+        //map the data table onto an object -> in this case a User Object
+        for (int i = 0; i < dataTable.height(); i++){
+            String firstName = dataTable.row(i).get(0);
+            String secondName = dataTable.row(i).get(1);
+            String email = dataTable.row(i).get(2);
+
+            User mUser = new User(firstName, secondName, email);
+            myUsersList.add(mUser); // add my user object into myUsersList
+        }
+
+        //for each loop -> for each element in the array run this loop
+        for (User user: myUsersList){
+            System.out.println(user.getUserFirstName());
+            System.out.println(user.getUserSecondName());
+            System.out.println(user.getUserEmail());
+            System.out.println("---From Second Loop---");
+        }
     }
 
 
--- src/test/resources/features/LoginFeature.feature
+++ src/test/resources/features/LoginFeature.feature
@@ -2,6 +2,10 @@ Feature: Login Screen Testing
 
   Scenario:  Positive Login Tests
     Given My browser is up and running and website is loaded 123
-    Then Go to site Enter Username "salam101"
-    Then Enter Password "myPassword" and "MysecondPassword"
-    Then validate success login
+    Then Test using following data
+        |Abdul          |salam          |salam@gmail.com    |
+        |nick           |allen          |nick@gmail.com     |
+        |bread          |crum           |bread@gmail.com    |
+        |horse          |man            |horse@gmail.com    |
+        |cat            |evil           |cat@gmail.com      |
+        |dog            |friend         |dog@gmail.com      |
