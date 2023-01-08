fdb11e74224bc381ab02b19f0ff9aacf5dfe5c2f
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-01-08 21:06:31 +0000

    updated for pull request

--- src/test/java/com/salam/selenium/tests/CucumberTestsExample.java
+++ src/test/java/com/salam/selenium/tests/CucumberTestsExample.java
@@ -11,21 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
 
-/**
- * Top Bar buttons -Home shop etc
- * Three tabs
- * Title text
- * back to our shop
- * quick links
- *
- *
- * Test static texts on these componenets
- * Test Navigation using top bar
- */
 public class CucumberTestsExample {
     DataMapperUtility dataMapperUtility = new DataMapperUtility();
 
-    WebDriver webDriver = new ChromeDriver();
+    protected WebDriver webDriver = new ChromeDriver();
 
     @Given("My browser is up and running and website is loaded {int}")
     public void my_browser_is_up_and_running_and_website_is_loaded(Integer int1) {
@@ -40,7 +29,6 @@ public class CucumberTestsExample {
     public void test_using_following_data(DataTable dataTable) {
 
         ArrayList<User> myUsersList =  dataMapperUtility.mapDataTableToArrayOfUsers(dataTable);
-
     }
 
     @Then("test users data on contact page")
---
+++ src/test/java/com/salam/selenium/tests/Debugger.java
@@ -0,0 +1,27 @@
+package com.salam.selenium.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class Debugger {
+
+
+    @Test
+    public void something(){
+
+        int x = 1;
+
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+        x = x + 2;
+
+
+    }
+}
